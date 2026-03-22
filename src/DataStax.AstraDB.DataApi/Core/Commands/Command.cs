37dbadc40805bca530b9e7262857c9674c6a6c78
toptobes <kavinpg@gmail.com>
2026-03-22 13:40:35 -0500

    why on earth is Test_DoubleAndFloatConverters failing

--- src/DataStax.AstraDB.DataApi/Core/Commands/Command.cs
+++ src/DataStax.AstraDB.DataApi/Core/Commands/Command.cs
@@ -401,7 +401,7 @@ internal class Command
                         responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                         MaybeLogDebugMessage("Response Status Code: {StatusCode}", response.StatusCode);
                         MaybeLogDebugMessage("Content: {Content}", responseContent);
-                        throw new HttpRequestException($"Request failed with status code {response.StatusCode}.");
+                        throw new HttpRequestException($"Request failed with status code {response.StatusCode} with  content {responseContent}.");
                     }
                 }
                 responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
