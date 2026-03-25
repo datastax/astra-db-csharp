947d4fe0fd235e004f7ed0f19797dec0ed81deb9
Stephen Hewitt <stephenh@sembit.com>
2026-03-25 13:18:00 -0700

    additions and corrections to xml comments

--- src/DataStax.AstraDB.DataApi/SerDes/TableUnknownIndexAPISupport.cs
+++ src/DataStax.AstraDB.DataApi/SerDes/TableUnknownIndexAPISupport.cs
@@ -20,16 +20,25 @@ using System.Text.Json.Serialization;
 namespace DataStax.AstraDB.DataApi.Tables;
 
 /// <summary>
-/// Configuration used to create a text index on a table column
+/// API capability details returned for a table index type that does not map to a known SDK definition.
 /// </summary>
 public class TableUnknownIndexAPISupport
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether the Data API reports support for index creation.
+    /// </summary>
     [JsonPropertyName("createIndex")]
     public bool CreateIndex { get; set; }
 
+    /// <summary>
+    /// Gets or sets a value indicating whether the Data API reports support for filtering with this index.
+    /// </summary>
     [JsonPropertyName("filter")]
     public bool Filter { get; set; }
 
+    /// <summary>
+    /// Gets or sets the CQL definition returned by the Data API for this index.
+    /// </summary>
     [JsonPropertyName("cqlDefinition")]
     public string CQLDefinition { get; set; }
 }
